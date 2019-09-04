8592c36a6a0faec651daf01a9cdfc33cb4a9a2ef
gitlab.ci <gitlab.ci@support.envisioncn.com>
2019-09-04 11:54:00 +0800

    Update.

--- conf.py
+++ conf.py
@@ -123,8 +123,8 @@ html_theme_options = {
 
 }
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
+# Add any paths that contain custom static files (such as style sheets),
+# relative to this directory. They are copied after the built-in static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
